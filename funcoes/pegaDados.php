fd7c77873732d4c51cf4eb1e54f4e0e0b6f1299f
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2023-02-27 19:42:37 -0300

    Update pegaDados.php

--- funcoes/pegaDados.php
+++ funcoes/pegaDados.php
@@ -5,9 +5,10 @@
         $resultado = mysqli_query($conexao, $sql);
         $dados = mysqli_fetch_array($resultado);
         return $dados;*/
-        $api_url = "https://db.ygoprodeck.com/api/v7/cardinfo.php?language=pt&fname=$busca";//URL da API
+        $api_url = "https://db.ygoprodeck.com/api/v7/cardinfo.php?language=pt&fname=$busca&num=50&offset=0";//URL da API
         $json_data = file_get_contents($api_url);//Pega os dados da API
         $response_data = json_decode($json_data);//Decodifica os dados da API
         $cards = $response_data->data;//Pega os dados da API
         return $cards;//Retorna os dados da API
     }
+?>
