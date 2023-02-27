b1e0513914b80188b48b046eddb458e7b7fd31b5
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2023-02-27 19:42:42 -0300

    Update getData.php

--- getData.php
+++ getData.php
@@ -1,5 +1,5 @@
 <?php
-    $api_url = 'https://db.ygoprodeck.com/api/v7/cardinfo.php';//api url
+    $api_url = 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=50&offset=0';//api url
     $json_data = file_get_contents($api_url);// obter dados da API
     $response_data = json_decode($json_data);// decodificar dados da API
     $user_data = $response_data->data;// obter dados da API
